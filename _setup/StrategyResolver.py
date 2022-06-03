f3b6f06c1c786a10df8c97a4d5ad0bd439cdd75a
sajanrajdev <sajanrajdev10@gmail.com>
2022-06-03 16:31:21 -0400

    feat: resolver initial impl

--- _setup/StrategyResolver.py
+++ _setup/StrategyResolver.py
@@ -1,5 +1,6 @@
 from helpers.StrategyCoreResolver import StrategyCoreResolver
 from rich.console import Console
+from brownie import interface
 
 console = Console()
 
@@ -34,20 +35,48 @@ class StrategyResolver(StrategyCoreResolver):
         """
         assert True
 
-    # def confirm_harvest(self, before, after, tx):
-    #     """
-    #     Verfies that the Harvest produced yield and fees
-    #     NOTE: This overrides default check, use only if you know what you're doing
-    #     """
-    #     console.print("=== Compare Harvest ===")
-    #     self.manager.printCompare(before, after)
-    #     self.confirm_harvest_state(before, after, tx)
+    def confirm_harvest(self, before, after, tx):
+        """
+        Verfies that the Harvest produced yield and fees
+        NOTE: This overrides default check, use only if you know what you're doing
+        """
+        console.print("=== Compare Harvest ===")
+        self.manager.printCompare(before, after)
+        self.confirm_harvest_state(before, after, tx)
 
-    #     valueGained = after.get("sett.getPricePerFullShare") > before.get(
-    #         "sett.getPricePerFullShare"
-    #     )
+        autocompoundBps = self.manager.strategy.autocompoundBps()
+        emitBps = self.manager.strategy.emitBps()
+        treasuryBps = self.manager.strategy.treasuryBps()
 
-    #     assert True
+        if autocompoundBps > 0:
+            # Check that we autocompounded
+            assert after.get("sett.getPricePerFullShare") > before.get(
+                "sett.getPricePerFullShare"
+            )
+            # Check that we re-deposit tokens
+            assert after.balances("want", "baseRewardsPool") > before.balances(
+                "want", "baseRewardsPool"
+            )
+        
+        if emitBps > 0:
+            # Check that wBTC was distributed to Locker
+            assert after.balances("wbtc", "xCitadelLocker") > before.balances(
+                "wbtc", "xCitadelLocker"
+            )
+            # Check event
+            event = tx.events["RewardAdded"][0]
+            assert event["account"] == self.manager.strategy.address
+            assert event["_token"] == self.manager.strategy.wbtc()
+            assert event["_reward"] > 0
+            assert event["_dataTypeHash"] == "0xaf388c3c3157dbb1999fecd2348a129dd286852ceddb9352feabbffbac7ca99b"
+
+        if treasuryBps > 0:
+            # Check that wBTC was distributed to Citadel's treasury
+            assert after.balances("wbtc", "citadelTreasury") > before.balances(
+                "wbtc", "citadelTreasury"
+            )
+
+        assert True
 
     def confirm_tend(self, before, after, tx):
         """
@@ -58,3 +87,36 @@ class StrategyResolver(StrategyCoreResolver):
         (Strategy Must Implement)
         """
         assert True
+
+    def add_entity_balances_for_tokens(self, calls, tokenKey, token, entities):
+        entities["strategy"] = self.manager.strategy.address
+        entities["baseRewardsPool"] = self.manager.strategy.baseRewardsPool()
+        entities["xCitadelLocker"] = self.manager.strategy.xCitadelLocker()
+        entities["citadelTreasury"] = self.manager.strategy.citadelTreasury()
+
+        super().add_entity_balances_for_tokens(calls, tokenKey, token, entities)
+        return calls
+
+    def add_balances_snap(self, calls, entities):
+        super().add_balances_snap(calls, entities)
+        strategy = self.manager.strategy
+
+        crv = interface.IERC20(strategy.crv())
+        cvx = interface.IERC20(strategy.cvx())
+        wbtc = interface.IERC20(strategy.wbtc())
+        ctdl = interface.IERC20(strategy.ctdl())
+
+        ## Get the booster for this strat
+        booster = interface.IBooster(strategy.booster())
+        ## So we can get the lpToken associated
+        convexLpToken = interface.IERC20(booster.poolInfo(strategy.pid())["token"])
+
+        calls = self.add_entity_balances_for_tokens(
+            calls, "convexLpToken", convexLpToken, entities
+        )
+        calls = self.add_entity_balances_for_tokens(calls, "crv", crv, entities)
+        calls = self.add_entity_balances_for_tokens(calls, "cvx", cvx, entities)
+        calls = self.add_entity_balances_for_tokens(calls, "wbtc", wbtc, entities)
+        calls = self.add_entity_balances_for_tokens(calls, "ctdl", ctdl, entities)
+
+        return calls
