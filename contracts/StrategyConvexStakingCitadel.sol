9ebd726b0ae2bfeb5efbd7a6cda4f8d8c5a3c920
sajanrajdev <sajanrajdev10@gmail.com>
2022-06-03 10:26:16 -0400

    fix: protected tokens2

--- contracts/StrategyConvexStakingCitadel.sol
+++ contracts/StrategyConvexStakingCitadel.sol
@@ -144,7 +144,7 @@ contract StrategyConvexStakingCitadel is
     /// @notice It's very important all tokens that are meant to be in the strategy to be marked as protected
     /// @notice this provides security guarantees to the depositors they can't be sweeped away
     function getProtectedTokens() public view virtual override returns (address[] memory) {
-        address[] memory protectedTokens = new address[](2);
+        address[] memory protectedTokens = new address[](5);
         protectedTokens[0] = want;
         protectedTokens[1] = address(wbtc);
         protectedTokens[2] = address(ctdl);
