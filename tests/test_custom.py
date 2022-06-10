195863859a2f014f95b04773a921de7a6ef72d04
sajanrajdev <sajanrajdev10@gmail.com>
2022-06-10 14:59:10 -0400

    test: different distribution splits

--- tests/test_custom.py
+++ tests/test_custom.py
@@ -5,6 +5,7 @@ import time
 from helpers.time import days
 from rich.console import Console
 from _setup.config import PID
+from helpers.SnapshotManager import SnapshotManager
 
 console = Console()
 
@@ -49,6 +50,45 @@ def test_expected_CVX_rewards_match_minted(deployer, vault, strategy, want, keep
     # Check that CVX amount calculating function matches the result
     assert cvx_amount == strategy.getMintableCVXRewards(crv_amount)
 
-    # First transfer from Harvest function is emitted by cvx._mint()
+    # First Transfer event from harvest() function is emitted by cvx._mint()
     tx = strategy.harvest({"from": keeper})
     assert tx.events["Transfer"][0]["value"] == cvx_amount
+
+
+def test_different_distribution_split_results(deployer, vault, strategy, want, keeper, governance):
+    state_setup(deployer, vault, strategy, want, keeper)
+
+    snap = SnapshotManager(vault, strategy, "StrategySnapshot")
+
+    # Attempt to change ratio to more or less than expected
+    with brownie.reverts("Invalid Total Ratio"):
+        strategy.setRewardsManagementRatio(9000, 1000, 1000, {"from": governance}) # Add to 11000
+
+    with brownie.reverts("Invalid Total Ratio"):
+        strategy.setRewardsManagementRatio(0, 0, 1000, {"from": governance}) # Add to 1000
+
+    # Attempt set emitBPS to higher than allowed (20%)
+    with brownie.reverts("Invalid Emit BPS"):
+        strategy.setRewardsManagementRatio(5000, 3000, 2000, {"from": governance})
+
+    # Test different distribution cases
+    # NOTE: The resolver holds the appropiate checks per case upon harvest.
+    splits = [
+        [10000, 0, 0],
+        [0, 0, 10000],
+        [7000, 2000, 1000],
+        [0, 2000, 8000]
+    ]
+
+    chain.snapshot()
+    for split in splits:
+        strategy.setRewardsManagementRatio(
+            split[0],
+            split[1],
+            split[2],
+            {"from": governance}
+        )
+        snap.settHarvest({"from": keeper})
+        chain.revert()
+
+
